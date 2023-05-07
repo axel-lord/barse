a1d27fb6640590e9b4edba52b97b9169271bb5d7
Axel Lord <axel.rinkaby@gmail.com>
2023-05-07 20:14:34 +0200

    Replaced option with result in parse common

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,8 @@
-use std::{any, array::TryFromSliceError, num::TryFromIntError};
+use std::{any, array::TryFromSliceError, borrow::Cow, fmt::Debug, num::TryFromIntError};
 
 use thiserror::Error;
 
-#[derive(Debug, Clone, Copy, Error)]
+#[derive(Debug, Error)]
 pub enum Error {
     #[error("could not find flag of type {0}")]
     FlagNotFound(&'static str),
@@ -10,6 +10,8 @@ pub enum Error {
     SliceFailure,
     #[error("a checked operation failed")]
     CheckedOperation,
+    #[error("an unsupported value in a flag vas given, hint \"{0}\", value {1:?}")]
+    UnsupportedFlagValue(Cow<'static, str>, Box<dyn Debug + Send + Sync>),
 
     #[error(transparent)]
     TryFromIntError(#[from] TryFromIntError),
