fea510bba345a5004694c9586d3c5eafb2e1bc6d
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2023-05-10 22:35:08 +0200

    Added flag helper attribute to barse-derive

--- barse-derive/src/from_byte_reader.rs
+++ barse-derive/src/from_byte_reader.rs
@@ -1,10 +1,11 @@
 use proc_macro2::{Ident, Span, TokenStream};
-use quote::{format_ident, quote, quote_spanned, ToTokens};
-use syn::{spanned::Spanned, Data, DeriveInput, Lifetime};
+use quote::{quote, quote_spanned, ToTokens};
+use syn::{
+    parse::Parser, punctuated::Punctuated, spanned::Spanned, Data, DeriveInput, Expr, Lifetime,
+    MetaNameValue, Token,
+};
 
-pub fn mangle(ident: &Ident) -> Ident {
-    format_ident!("__parse_derive_{ident}")
-}
+use crate::{dyn_mangle, static_mangle};
 
 fn field_init(reader: &Ident) -> TokenStream {
     quote! {
@@ -12,6 +13,7 @@ fn field_init(reader: &Ident) -> TokenStream {
     }
 }
 
+#[allow(clippy::too_many_lines)]
 pub fn impl_trait(ast: &DeriveInput) -> Result<TokenStream, TokenStream> {
     let name = &ast.ident;
 
@@ -22,42 +24,127 @@ pub fn impl_trait(ast: &DeriveInput) -> Result<TokenStream, TokenStream> {
         });
     };
 
-    let reader = mangle(&Ident::new("reader", Span::call_site()));
+    let reader = static_mangle("reader");
     let input = {
-        let ident = mangle(&Ident::new("input", Span::call_site()));
+        let ident = static_mangle("input");
         Lifetime {
             apostrophe: Span::call_site(),
             ident,
         }
     };
+    let attr_ident = Ident::new("barse", Span::call_site());
+    let flag_ident = Ident::new("flag", Span::call_site());
 
     let body: TokenStream = match data_struct.fields {
         syn::Fields::Named(ref fields) => {
-            let field_init =
-                std::iter::from_fn(|| Some(field_init(&reader))).take(fields.named.len());
+            let mut body = TokenStream::new();
+            let mut return_value = TokenStream::new();
 
-            let field_names = fields
-                .named
-                .iter()
-                .map(|field| {
-                    field.ident.as_ref().ok_or_else(|| {
-                        let span = field.span();
-                        quote_spanned!(span=> compile_error!("unnamed field in non-tuple struct"))
-                    })
-                })
-                .collect::<Result<Vec<_>, _>>()?;
+            for field in &fields.named {
+                let name = field.ident.as_ref().ok_or_else(|| {
+                    let span = field.span();
+                    quote_spanned!(span=> compile_error!("unnamed field in non-tuple struct"))
+                })?;
 
-            quote! {
-                Ok(Self{#(#field_names: #field_init),*} )
+                let mangled_name = dyn_mangle(name);
+                let mut flags = Vec::new();
+                for attr in &field.attrs {
+                    if attr.path().is_ident(&attr_ident) {
+                        continue;
+                    };
+
+                    let values = match &attr.meta {
+                        syn::Meta::Path(_) => {
+                            let span = attr.span();
+                            return Err(quote_spanned! {
+                                span=> compile_error!("barse attributes should be a list of names and values")
+                            });
+                        }
+                        syn::Meta::List(list) => {
+                            let parser = Punctuated::<MetaNameValue, Token![,]>::parse_terminated;
+                            let list = parser
+                                .parse2(list.tokens.clone())
+                                .map_err(syn::Error::into_compile_error)?;
+                            let iter = list.into_iter();
+                            either::Either::Left(iter)
+                        }
+                        syn::Meta::NameValue(value) => {
+                            let iter = std::iter::once(value.clone());
+                            either::Either::Right(iter)
+                        }
+                    };
+
+                    for item in values {
+                        if item.path.is_ident(&flag_ident) {
+                            let Expr::Path(path) = item.value else {
+                                let span = item.value.span();
+                                return Err(quote_spanned!{
+                                    span=> compile_error!("only identifiers are accepted as flag values")
+                                });
+                            };
+
+                            let Some(ident) = path.path.get_ident() else {
+                                let span = path.span();
+                                return Err(quote_spanned!{
+                                    span=> compile_error!("flag value should be a single identifier")
+                                });
+                            };
+
+                            flags.push(dyn_mangle(ident));
+                        }
+                    }
+                }
+
+                let field_reader = if flags.is_empty() {
+                    quote!(&mut #reader)
+                } else {
+                    quote! {
+                        ::barse::FlagByteReader::new(&mut #reader, [#(& #flags),*])
+                    }
+                };
+
+                let init = quote! {
+                    ::barse::FromByteReader::from_byte_reader(#field_reader)?
+                };
+
+                // Add variable for this field
+                quote!(let #mangled_name = #init;).to_tokens(&mut body);
+
+                // Add this field to return value
+                quote!(#name: #mangled_name,).to_tokens(&mut return_value);
             }
+
+            // Add return value to body
+            quote! {
+                Ok(Self { #return_value })
+            }
+            .to_tokens(&mut body);
+
+            // return body
+            body
         }
         syn::Fields::Unnamed(ref fields) => {
-            let field_init =
-                std::iter::from_fn(|| Some(field_init(&reader))).take(fields.unnamed.len());
+            let mut body = TokenStream::new();
+            let mut return_value = TokenStream::new();
 
-            quote! {
-                Ok(Self(#(#field_init),*))
+            for (field_num, _) in fields.unnamed.iter().enumerate() {
+                let mangled_name =
+                    dyn_mangle(&Ident::new(&format!("{field_num}"), Span::call_site()));
+
+                let init = field_init(&reader);
+
+                // Ad variable for this field
+                quote!(let #mangled_name = #init;).to_tokens(&mut body);
+
+                // Ad this field to return value
+                quote!(#mangled_name,).to_tokens(&mut return_value);
             }
+
+            // Add retuen value to body
+            quote!(Ok(Self(#return_value))).to_tokens(&mut body);
+
+            // return body
+            body
         }
         syn::Fields::Unit => quote! {Ok(Self)},
     };
