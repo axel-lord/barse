81e03fcc95bc449e452420f08f70c15fb6b78e1c
Axel Lord <axel.rinkaby@gmail.com>
2023-05-12 00:38:59 +0200

    Moved most of barse-derive to barse-derive-impl to allow for tests

---
+++ barse-derive-impl/README.md
@@ -0,0 +1,5 @@
+## barse-derive-impl
+Implementation for proc macro crate for my binary file parsing libray barse. Needed due to testing.
+
+## Licensing
+This project is licensed under MIT or Apache-2.0 use whichever you fancy.
--- barse-derive/src/lib.rs
+++ barse-derive/src/lib.rs
@@ -49,126 +49,28 @@
     missing_docs
 )]
 
-use std::fmt;
-
 use proc_macro::TokenStream;
-use proc_macro2::Ident;
-use quote::{format_ident, quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, DeriveInput, FnArg, ItemFn, Type, TypeReference};
-
-mod condition;
-mod from_byte_reader;
-mod size_query;
-mod vec_len_query;
-
-fn simplify_result<T>(res: Result<T, T>) -> T {
-    match res {
-        Ok(t) | Err(t) => t,
-    }
-}
-
-fn dyn_mangle(ident: &Ident) -> Ident {
-    format_ident!("__dyn_barse_derive_i{ident}")
-}
-
-fn dyn_mangle_display<D>(disp: D) -> Ident
-where
-    D: fmt::Display,
-{
-    format_ident!("__dyn_disp_barse_derive_i{disp}")
-}
-
-fn static_mangle(ident: &str) -> Ident {
-    format_ident!("__static_barse_derive_i{ident}")
-}
 
 /// Derive a `FromByteReader` implementation.
 #[proc_macro_derive(FromByteReader, attributes(barse))]
 pub fn derive_from_byte_reader(item: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(item as DeriveInput);
-    simplify_result(from_byte_reader::impl_trait(&ast)).into()
+    barse_derive_impl::derive_from_byte_reader(item.into()).into()
 }
 
 /// Create a `ByteSizeQuery` implementor from a function.
 #[proc_macro_attribute]
 pub fn byte_size_query(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let name = parse_macro_input!(attr as Ident);
-    let body = parse_macro_input!(item as ItemFn);
-
-    let gen_trait = simplify_result(size_query::generate_impl(&name, &body));
-
-    quote! {
-        #body
-        #gen_trait
-    }
-    .into()
+    barse_derive_impl::byte_size_query(attr.into(), item.into()).into()
 }
 
 /// Create a Condition implementor from a function.
 #[proc_macro_attribute]
 pub fn condition(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let name = parse_macro_input!(attr as Ident);
-    let body = parse_macro_input!(item as ItemFn);
-
-    let gen_trait = simplify_result(condition::generate_impl(&name, &body));
-
-    quote! {
-        #body
-        #gen_trait
-    }
-    .into()
+    barse_derive_impl::condition(attr.into(), item.into()).into()
 }
 
 /// Create a `VecLenQuery` implementor from a function.
 #[proc_macro_attribute]
 pub fn vec_len_query(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let name = parse_macro_input!(attr as Ident);
-    let body = parse_macro_input!(item as ItemFn);
-
-    let gen_trait = simplify_result(vec_len_query::generate_impl(&name, &body));
-
-    quote! {
-        #body
-        #gen_trait
-    }
-    .into()
-}
-
-fn fn_name_and_type(body: &ItemFn) -> Result<(&Ident, &Type), proc_macro2::TokenStream> {
-    let fn_name = &body.sig.ident;
-
-    if !body.sig.generics.params.is_empty() {
-        let span = body.sig.generics.span();
-        return Err(quote_spanned! {
-            span=> compile_error!("annotated function can not have generic params or lifetimes")
-        });
-    }
-
-    if body.sig.inputs.len() != 1 {
-        let span = body.sig.inputs.span();
-        return Err(quote_spanned! {
-            span=> compile_error!("annotated function should have one and only have one parameter")
-        });
-    }
-
-    let Some(FnArg::Typed(flag_param)) = &body.sig.inputs.first() else {
-        let span = body.sig.inputs.span();
-        return Err(quote_spanned!{
-            span=> compile_error!("annotated function should have a non-self parameter")
-        });
-    };
-
-    let Type::Reference(TypeReference {
-        lifetime: None,
-        mutability: None,
-        elem: ty,
-        ..
-    }) = &*flag_param.ty else {
-        let span = flag_param.span();
-        return Err(quote_spanned!{
-            span=> compile_error!("annotaded function should have it's param be a immutable reference with no specified lifetime")
-        });
-    };
-
-    Ok((fn_name, ty))
+    barse_derive_impl::vec_len_query(attr.into(), item.into()).into()
 }
