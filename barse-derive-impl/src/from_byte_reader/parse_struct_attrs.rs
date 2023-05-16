6a2c54b9bbd4959bca9e043fe0f7d8554e0a6dbd
Axel Lord <axel.rinkaby@gmail.com>
2023-05-17 00:49:27 +0200

    Started adding more struct attributes

--- barse-derive-impl/src/from_byte_reader/parse_struct_attrs.rs
+++ barse-derive-impl/src/from_byte_reader/parse_struct_attrs.rs
@@ -2,11 +2,16 @@ use proc_macro2::TokenStream;
 use quote::quote_spanned;
 use syn::{spanned::Spanned, Attribute, Expr, ExprLit, Meta};
 
-use super::{invalid_attr_type, parse_attrs, Ctx};
+use super::{
+    ident_map, invalid_attr_type, parse_attrs, parse_fields::parse_field_attrs::ParseAs, Ctx,
+};
 
 #[derive(Default)]
 pub struct StructAttrs {
     pub error: Option<syn::Type>,
+    pub parse_as: ParseAs,
+    pub with: Option<syn::Type>,
+    pub reveal: Option<syn::Pat>,
 }
 
 pub fn parse_struct_attrs(attrs: &[Attribute], ctx: &Ctx) -> Result<StructAttrs, TokenStream> {
@@ -17,19 +22,33 @@ pub fn parse_struct_attrs(attrs: &[Attribute], ctx: &Ctx) -> Result<StructAttrs,
             Meta::Path(_) => (),
             Meta::NameValue(item) => {
                 let Expr::Lit(ExprLit { lit: syn::Lit::Str(lit_str), .. }) = item.value
-                    else {
-                        let span = item.value.span();
-                        return Err(quote_spanned!{
-                            span=> compile_error!("value should be a string literal")
-                        });
-                    };
+                else {
+                    let span = item.value.span();
+                    return Err(quote_spanned!{
+                        span=> compile_error!("value should be a string literal")
+                    });
+                };
 
-                if item.path.is_ident(&ctx.error_attr) {
-                    let ty = lit_str
-                        .parse::<syn::Type>()
-                        .map_err(syn::Error::into_compile_error)?;
-                    struct_attrs.error = Some(ty);
-                }
+                ident_map!(item.path, {
+                    &ctx.error_attr => {
+                        let ty = lit_str
+                            .parse()
+                            .map_err(syn::Error::into_compile_error)?;
+                        struct_attrs.error = Some(ty);
+                    },
+                    &ctx.from_attr => {
+                        let path = lit_str
+                            .parse()
+                            .map_err(syn::Error::into_compile_error)?;
+                        struct_attrs.parse_as = ParseAs::Yes(path);
+                    },
+                    &ctx.try_from_attr => {
+                        let path = lit_str
+                            .parse()
+                            .map_err(syn::Error::into_compile_error)?;
+                        struct_attrs.parse_as = ParseAs::Try(path);
+                    },
+                });
             }
         }
     }
