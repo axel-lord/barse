8a234d20973ed9db46977c7eab4cfd31e60bbe05
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-12-05 19:22:36 +0100

    start of new implementation

--- barse/src/lib.rs
+++ barse/src/lib.rs
@@ -1,90 +1,41 @@
-//! Library for parsing binary data.
+#![doc = include_str!("../README.md")]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#![warn(
-    clippy::all,
-    clippy::pedantic,
-    clippy::perf,
-    clippy::style,
-    clippy::disallowed_types,
-    clippy::indexing_slicing,
-    clippy::arithmetic_side_effects,
-    clippy::clone_on_ref_ptr,
-    clippy::create_dir,
-    clippy::default_numeric_fallback,
-    clippy::empty_drop,
-    clippy::empty_structs_with_brackets,
-    clippy::unwrap_used,
-    clippy::expect_used,
-    clippy::float_equality_without_abs,
-    clippy::float_cmp,
-    clippy::float_cmp_const,
-    clippy::format_push_string,
-    clippy::get_unwrap,
-    clippy::if_then_some_else_none,
-    clippy::impl_trait_in_params,
-    clippy::mixed_read_write_in_expression,
-    clippy::mod_module_files,
-    clippy::multiple_unsafe_ops_per_block,
-    clippy::undocumented_unsafe_blocks,
-    clippy::partial_pub_fields,
-    clippy::panic,
-    clippy::semicolon_if_nothing_returned,
-    clippy::semicolon_inside_block,
-    clippy::str_to_string,
-    clippy::todo,
-    clippy::try_err,
-    clippy::unneeded_field_pattern,
-    clippy::unseparated_literal_suffix,
-    clippy::fallible_impl_from,
-    clippy::future_not_send,
-    clippy::option_if_let_else,
-    clippy::or_fun_call,
-    clippy::path_buf_push_overwrite,
-    clippy::redundant_pub_crate,
-    clippy::redundant_allocation,
-    clippy::significant_drop_tightening,
-    clippy::useless_let_if_seq,
-    rustdoc::all,
-    missing_copy_implementations,
-    missing_debug_implementations,
-    missing_docs
-)]
+mod sealed {
+    //! [Sealed] trait.
 
-// Utility
-pub use error::Error;
+    /// Trait used to prevent implementations.
+    pub trait Sealed {}
+}
 
-// Traits
-pub use byte_read::ByteRead;
-pub use from_byte_reader::FromByteReader;
-pub use from_byte_reader_with::FromByteReaderWith;
+pub use endian::Endian;
 
-// ByteRead
-
-#[cfg(feature = "derive")]
-pub use barse_derive::FromByteReader;
-
-/// Result type in use by crate.
-pub type Result<T, E = error::Error> = std::result::Result<T, E>;
-
-// Utility
 pub mod endian;
-mod error;
 
-// Traits
-mod byte_read;
-mod from_byte_reader;
-mod from_byte_reader_with;
+#[cfg(feature = "std")]
+mod if_std {
+    //! Trait implementations and types used with std feature.
+}
 
-// FromByteReader
-pub mod from_reader;
+/// Source of bytes for reading.
+pub trait ByteSource {}
 
-// ByteRead
-pub mod reader;
+/// Sink for writing of bytes.
+pub trait ByteSink {}
 
-// trait wrapping types
-pub mod wrap;
+/// Trait to serialize and deserialize from binary data.
+pub trait Barse: Sized {
+    type Err;
 
-pub mod prelude {
-    //! Prelude module with all traits in use by crate.
-    pub use super::{ByteRead, FromByteReader, FromByteReaderWith};
+    /// Read an instnce from source with given endianess.
+    ///
+    /// # Errors
+    /// If Soure or implementation errors.
+    fn read<E: Endian>(from: impl ByteSource) -> Result<Self, Self::Err>;
+
+    /// Write an instance to a sink with given endianess.
+    ///
+    /// # Errors
+    /// If Sink or implementation errors.
+    fn write<E: Endian>(&self, to: impl ByteSink) -> Result<(), Self::Err>;
 }
