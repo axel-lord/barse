50ca79d07ef29cb06c87436022468e56267c938f
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-12-08 14:40:06 +0100

    split out bytemuck and zerocopy implementations to files

---
+++ barse/src/util/bytemuck.rs
@@ -0,0 +1,54 @@
+//! [Barse] implementations using bytemuck.
+
+use ::bytemuck::{AnyBitPattern, NoUninit};
+
+use crate::Barse;
+
+/// Value implementing [AnyBitPattern] and [NoUninit].
+///
+/// Endianess will be platform specific ([Native][crate::endian::Native]) in all cases.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct UseAnyBitPattern<T>(T);
+
+impl<T> UseAnyBitPattern<T>
+where
+    T: AnyBitPattern + NoUninit + Barse,
+{
+    /// Construct a new [UseAnyBitPattern] from a value.
+    pub const fn new(value: T) -> Self {
+        Self(value)
+    }
+
+    /// Unwrap [UseAnyBitPattern] to wrapped value.
+    pub const fn into_inner(self) -> T {
+        let Self(value) = self;
+        value
+    }
+}
+
+impl<T> Barse for UseAnyBitPattern<T>
+where
+    T: AnyBitPattern + NoUninit,
+{
+    fn read<E, B>(from: &mut B) -> Result<Self, crate::Error<B::Err>>
+    where
+        E: crate::Endian,
+        B: crate::ByteSource,
+    {
+        let mut value = T::zeroed();
+
+        from.read_slice(::bytemuck::bytes_of_mut(&mut value))?;
+
+        Ok(Self(value))
+    }
+
+    fn write<E, B>(&self, to: &mut B) -> Result<(), crate::Error<B::Err>>
+    where
+        E: crate::Endian,
+        B: crate::ByteSink,
+    {
+        to.write_slice(::bytemuck::bytes_of(&self.0))?;
+        Ok(())
+    }
+}
+
