c82b7cabd26f0bff6971dee42b5290ca95de2bf8
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-12-16 18:39:46 +0100

    basic barse read derive complete

--- barse-derive-impl/src/barse_struct.rs
+++ barse-derive-impl/src/barse_struct.rs
@@ -7,7 +7,7 @@ use ::syn::{
     TypeParamBound, WhereClause,
 };
 
-use crate::barse_struct::struct_config::StructConfig;
+use crate::barse_struct::{field_config::FieldConfig, struct_config::StructConfig};
 
 mod struct_config;
 
@@ -17,6 +17,9 @@ mod field_config;
 ///
 /// # Errors
 /// Should derive not be possible.
+///
+/// # Panics
+/// On bad implementation.
 pub fn derive_barse_struct(mut item: ItemStruct) -> Result<TokenStream, ::syn::Error> {
     let StructConfig {
         barse_path,
@@ -31,6 +34,10 @@ pub fn derive_barse_struct(mut item: ItemStruct) -> Result<TokenStream, ::syn::E
         field_prefix,
     } = StructConfig::from_attrs(&item.attrs)?;
     let name = &item.ident;
+    let field_prefix = field_prefix.or_else(|| match item.fields {
+        ::syn::Fields::Unnamed(_) => Some(format_ident!("_")),
+        ::syn::Fields::Named(_) | ::syn::Fields::Unit => None,
+    });
 
     let r = ::rand::random::<u32>();
 
@@ -64,17 +71,66 @@ pub fn derive_barse_struct(mut item: ItemStruct) -> Result<TokenStream, ::syn::E
         }
     }
 
-    let read_body = item.fields.iter().map(|field| {
-
-    });
-
-    let (impl_generics, ty_generics, split_where_clause) = item.generics.split_for_impl();
-    let where_clause = where_clause.as_ref().or(split_where_clause);
-
     let e = format_ident!("__E_{r:X}");
     let b = format_ident!("__B_{r:X}");
     let to = format_ident!("__to_{r:x}");
     let from = format_ident!("__from_{r:x}");
+
+    let fields = item
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| {
+            let cfg = FieldConfig::from_attrs(&field.attrs);
+            let name = field.ident.as_ref().map_or_else(
+                || {
+                    field_prefix.as_ref().map_or_else(
+                        || panic!("field_prefix needs to exist for tuple structs"),
+                        |field_prefix| format_ident!("{field_prefix}{i}"),
+                    )
+                },
+                |ident| {
+                    field_prefix.as_ref().map_or_else(
+                        || ident.clone(),
+                        |field_prefix| format_ident!("{field_prefix}{ident}"),
+                    )
+                },
+            );
+            (field, cfg, name)
+        })
+        .collect::<Vec<_>>();
+
+    let read_body = fields
+        .iter()
+        .map(|(field, _cfg, name)| {
+            let ty = &field.ty;
+            quote! {
+                let #name = <#ty as #barse_path>::read::<#e, #b>(#from, ())?;
+            }
+        })
+        .collect::<TokenStream>();
+
+    let read_return = {
+        let fields = fields.iter().map(|(field, _cfg, name)| {
+            field.ident.as_ref().map_or_else(
+                || {
+                    quote! { #name, }
+                },
+                |ident| {
+                    quote! { #ident: #name, }
+                },
+            )
+        });
+
+        match item.fields {
+            ::syn::Fields::Named(_) => quote! {Ok(Self{#(#fields)*})},
+            ::syn::Fields::Unnamed(_) => quote! {Ok(Self(#(#fields)*))},
+            ::syn::Fields::Unit => quote! {Ok(Self)},
+        }
+    };
+
+    let (impl_generics, ty_generics, split_where_clause) = item.generics.split_for_impl();
+    let where_clause = where_clause.as_ref().or(split_where_clause);
 
     let with = with.unwrap_or_else(|| {
         let with = format_ident!("__with_{r:x}");
@@ -97,7 +153,8 @@ pub fn derive_barse_struct(mut item: ItemStruct) -> Result<TokenStream, ::syn::E
                 #e: #endian_path,
                 #b: #byte_source_path,
             {
-                todo!()
+                #read_body
+                #read_return
             }
 
             fn write<#e, #b>(&self, #to: &mut #b, #write_with) -> ::core::result::Result<(), #error_path::<#b::Err>>
