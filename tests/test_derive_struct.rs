91051b1f97621bdfdb999c1afd0cfb9c3aa11093
Axel Lord <axel.lord2000@gmail.com>
2024-12-18 20:50:27 +0100

    added test to check write-read with different types using the same barse layout

--- tests/test_derive_struct.rs
+++ tests/test_derive_struct.rs
@@ -3,20 +3,12 @@
 
 use ::barse::{
     self as barse2,
-    util::{SliceSink, SliceSrc},
-    ByteSinkExt, ByteSourceExt,
+    endian::Little,
+    util::{SliceSink, SliceSrc, UseEndian},
+    Barse, ByteSinkExt, ByteSourceExt,
 };
 
-#[derive(::barse::Barse, PartialEq, Debug)]
-#[barse(field_prefix = field_)]
-struct TestStruct {
-    a: f32,
-    b: i64,
-    c: u128,
-    d: [u8; 5],
-}
-
-#[derive(::barse::Barse)]
+#[derive(Barse)]
 #[barse(
     where(
         T: ::barse::Barse<ReadWith = (), WriteWith = ()>,
@@ -28,9 +20,18 @@ struct TestStruct {
 )]
 struct Wrap<T>(T);
 
-/// Entry
+/// Basic test.
 #[test]
 fn basic() {
+    #[derive(Barse, PartialEq, Debug)]
+    #[barse(field_prefix = field_)]
+    struct TestStruct {
+        a: f32,
+        b: i64,
+        c: u128,
+        d: [u8; 5],
+    }
+
     let test_struct = TestStruct {
         a: 5.6,
         b: -15,
@@ -45,4 +46,32 @@ fn basic() {
     let barsed_test_struct = SliceSrc::new(&buf).read_be::<TestStruct>().unwrap();
 
     assert_eq!(test_struct, barsed_test_struct);
+}
+
+/// Test of deterministic property with different types.
+#[test]
+fn deterministic() {
+    #[derive(Debug, PartialEq, Eq, Barse, Clone, Copy)]
+    struct A {
+        a: i16,
+        b: u16,
+        c: i32,
+    }
+    #[derive(Debug, PartialEq, Eq, Barse, Clone, Copy)]
+    struct B(i16, u16, i32);
+
+    let (a, b, c) = (-5, 67, -35_000);
+
+    let struct_a = A { a, b, c };
+    let struct_b = B(a, b, c);
+
+    let mut buf = [0u8; size_of::<A>()];
+
+    SliceSink::new(&mut buf)
+        .write_be(&UseEndian::<A, Little>::new(struct_a))
+        .unwrap();
+
+    let barsed_b = SliceSrc::new(&buf).read_le::<B>().unwrap();
+
+    assert_eq!(struct_b, barsed_b);
 }
