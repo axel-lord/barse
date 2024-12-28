bc803e714e1536bc792f761d67e2689064509975
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-12-28 22:36:07 +0100

    added functions to easily read/write values

--- barse/src/if_std.rs
+++ barse/src/if_std.rs
@@ -2,7 +2,11 @@
 
 use ::std::io::{Cursor, Read, Write};
 
-use crate::{ByteSink, ByteSource};
+use crate::{
+    endian::Native,
+    ext::{AsByteSink, AsByteSource},
+    Barse, ByteSink, ByteSource,
+};
 
 impl<A> ByteSource for Cursor<A>
 where
@@ -25,4 +29,36 @@ where
     fn write_slice(&mut self, buf: &[u8]) -> Result<(), Self::Err> {
         self.write_all(buf)
     }
+}
+
+#[cfg(feature = "ext")]
+/// Write a value using native endian to a [Write] implementor.
+///
+/// # Errors
+/// If [Barse] implementation errors.
+/// Or if the bytes resulting from it cannot be written.
+pub fn write_value<T, W>(value: &T, mut to: W) -> ::std::io::Result<()>
+where
+    T: Barse<WriteWith = ()>,
+    W: ::std::io::Write,
+{
+    use crate::WrappedErr;
+
+    T::write_with::<Native, _>(value, &mut to.as_byte_sink(), ()).map_err(WrappedErr::merge_into)
+}
+
+#[cfg(feature = "ext")]
+/// Read a value using native endian from a [Read] implementor.
+///
+/// # Errors
+/// If [Barse] implementation errors.
+/// Or if the bytes needed cannot be read.
+pub fn read_value<T, R>(mut from: R) -> ::std::io::Result<T>
+where
+    T: Barse<ReadWith = ()>,
+    R: ::std::io::Read,
+{
+    use crate::WrappedErr;
+
+    T::read_with::<Native, _>(&mut from.as_byte_source(), ()).map_err(WrappedErr::merge_into)
 }
