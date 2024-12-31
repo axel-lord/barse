5710a839b520d162f782250a7489bcb4f81d5908
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2024-12-31 14:55:26 +0100

    added Bytes ReadAs/WriteAs implementor and moved error types

--- barse/src/util/slice_sink.rs
+++ barse/src/util/slice_sink.rs
@@ -1,8 +1,6 @@
 //! [SliceSink] implementation.
 
-use ::core::fmt::Display;
-
-use crate::ByteSink;
+use crate::{util::error::SliceSinkFull, ByteSink};
 
 /// [ByteSink] implementor wrapping a slice.
 ///
@@ -21,26 +19,6 @@ impl<'src> SliceSink<'src> {
     /// Create a new [SliceSink] backed by given slice.
     pub const fn new(slice: &'src mut [u8]) -> Self {
         Self { slice, head: 0 }
-    }
-}
-
-/// Error returned by [ByteSink] implementation for [SliceSink] when no more bytes can be written
-/// to sink.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct SliceSinkFull;
-
-impl Display for SliceSinkFull {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str("a value was too large to be written to remaining length at head of SliceSink")
-    }
-}
-
-impl ::core::error::Error for SliceSinkFull {}
-
-impl From<SliceSinkFull> for crate::Error {
-    fn from(_value: SliceSinkFull) -> Self {
-        static ERR: SliceSinkFull = SliceSinkFull;
-        crate::Error::Dyn(&ERR)
     }
 }
 
