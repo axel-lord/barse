1b416003ee776eaf958e513f1662cef966437357
Axel Lord <axel.lord2000@gmail.com>
2025-01-06 14:04:00 +0100

    added box and cow impls

--- barse/src/if_alloc.rs
+++ barse/src/if_alloc.rs
@@ -2,7 +2,9 @@
 
 use ::core::convert::Infallible;
 
-use crate::{ByteSink, ByteSource};
+use alloc::{borrow::Cow, boxed::Box};
+
+use crate::{Barse, ByteSink, ByteSource};
 
 extern crate alloc;
 
@@ -15,7 +17,7 @@ impl ByteSink for alloc::vec::Vec<u8> {
     }
 }
 
-impl<Src> ByteSource for alloc::boxed::Box<Src>
+impl<Src> ByteSource for Box<Src>
 where
     Src: ByteSource,
 {
@@ -26,7 +28,7 @@ where
     }
 }
 
-impl<Sink> ByteSink for alloc::boxed::Box<Sink>
+impl<Sink> ByteSink for Box<Sink>
 where
     Sink: ByteSink,
 {
@@ -34,5 +36,74 @@ where
 
     fn write_slice(&mut self, buf: &[u8]) -> Result<(), Self::Err> {
         (**self).write_slice(buf)
+    }
+}
+
+impl<T> Barse for Box<T>
+where
+    T: Barse,
+{
+    type ReadWith = T::ReadWith;
+
+    type WriteWith = T::WriteWith;
+
+    #[inline]
+    fn read_with<E, B>(
+        from: &mut B,
+        with: Self::ReadWith,
+    ) -> Result<Self, crate::WrappedErr<B::Err>>
+    where
+        E: crate::Endian,
+        B: ByteSource,
+    {
+        T::read_with::<E, _>(from, with).map(Box::new)
+    }
+
+    #[inline]
+    fn write_with<E, B>(
+        &self,
+        to: &mut B,
+        with: Self::WriteWith,
+    ) -> Result<(), crate::WrappedErr<B::Err>>
+    where
+        E: crate::Endian,
+        B: ByteSink,
+    {
+        T::write_with::<E, _>(self, to, with)
+    }
+}
+
+impl<'a, T> Barse for Cow<'a, T>
+where
+    T: ToOwned + Barse + 'a,
+    T::Owned: Barse<ReadWith = T::ReadWith, WriteWith = T::WriteWith>,
+{
+    type ReadWith = <T::Owned as Barse>::ReadWith;
+
+    type WriteWith = <T::Owned as Barse>::WriteWith;
+
+    #[inline]
+    fn read_with<E, B>(
+        from: &mut B,
+        with: Self::ReadWith,
+    ) -> Result<Self, crate::WrappedErr<B::Err>>
+    where
+        E: crate::Endian,
+        B: ByteSource,
+    {
+        T::Owned::read_with::<E, _>(from, with).map(Cow::Owned)
+    }
+
+    #[inline]
+    fn write_with<E, B>(
+        &self,
+        to: &mut B,
+        with: Self::WriteWith,
+    ) -> Result<(), crate::WrappedErr<B::Err>>
+    where
+        E: crate::Endian,
+        B: ByteSink,
+    {
+        T::write_with::<E, _>(self, to, with)
     }
 }
